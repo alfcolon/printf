debb62c2ec1663f0db01fb4f7370785ff72b0da8
jmtes <juliennetesoro@gmail.com>
2019-03-18 19:50:19 +0000

    Create program cspec_x.c

---
+++ cspec_x.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "holberton.h"
+
+/**
+ * cspec_x - Handle lowercase hex specifiers
+ * @val: Unsigned decimal int to convert to base-16
+ * @buffer: Buffer to copy lowercase hex string into
+ *
+ * Return: None
+ */
+
+void cspec_x(void *val, char *buffer)
+{
+	unsigned int *valu = val;
+	unsigned int value = *valu;
+	char tmp_char;
+	int tmp_value;
+	char *st = buffer;
+	char *end = buffer;
+
+	char *digits = "0123456789abcdef";
+
+	do {
+		tmp_value = value;
+		value /= 16;
+		*st = digits[tmp_value % 16];
+		st++;
+	} while (value);
+
+	st--;
+
+	while (end < st)
+	{
+	tmp_char = *st;
+	*st-- = *end;
+	*end++ = tmp_char;
+	}
+}
