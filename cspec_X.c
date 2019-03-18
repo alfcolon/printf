d5a5d3bf11ac85e62c2c6fc693d15ececb687cc8
jmtes <juliennetesoro@gmail.com>
2019-03-18 19:55:32 +0000

    Create program cspec_X.c

---
+++ cspec_X.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "holberton.h"
+
+/**
+ * cspec_X - Handle uppercase hex specifiers
+ * @val: Unsigned decimal int to convert to base-16
+ * @buffer: Buffer to copy uppercase hex string into
+ *
+ * Return: None
+ */
+
+void cspec_X(void *val, char *buffer)
+{
+	unsigned int *valu = val;
+	unsigned int value = *valu;
+	char tmp_char;
+	int tmp_value;
+	char *st = buffer;
+	char *end = buffer;
+
+	char *digits = "0123456789ABCDEF";
+
+	do {
+		tmp_value = value;
+		value /= 16;
+		*st = digits[tmp_value % 16];
+		st++;
+	} while (value);
+
+	st--;
+
+	while (end < st)
+	{
+	tmp_char = *st;
+	*st-- = *end;
+	*end++ = tmp_char;
+	}
+}
