a69cb281a797013d41d29c16d6cac77f1b88cbb1
jmtes <juliennetesoro@gmail.com>
2019-03-18 19:40:39 +0000

    Create program cspec_o.c

---
+++ cspec_o.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "holberton.h"
+
+/**
+ * cspec_o - Handle octal format specifiers
+ * @val: Unsigned decimal int to convert to base-8
+ * @buffer: Buffer to copy octal string into
+ *
+ * Return: None
+ */
+
+void cspec_o(void *val, char *buffer)
+{
+  unsigned int *valu = val;
+  unsigned int value = *valu;
+  char tmp_char;
+  int tmp_value;
+
+  char *st = buffer;
+  char *end = buffer;
+  char *digits = "01234567";
+
+  do {
+    tmp_value = value;
+    value /= 8;
+    *st = digits[tmp_value % 8];
+    st++;
+  } while (value);
+
+  *st = '\0';
+  st--;
+
+  while (end < st)
+  {
+    tmp_char = *st;
+    *st-- = *end;
+    *end++ = tmp_char;
+  }
+}
