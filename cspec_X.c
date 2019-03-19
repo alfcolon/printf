97f0477ffde92cb4c9690e5af06f460b809c6675
jmtes <juliennetesoro@gmail.com>
2019-03-18 22:22:20 -0700

    Apply changes made to cspec_x.c

--- cspec_X.c
+++ cspec_X.c
@@ -16,27 +16,31 @@ void cspec_X(void *val, char *buffer)
 	unsigned int *valu = val;
 	unsigned int value = *valu;
 	char tmp_char;
-	int tmp_value;
-	char *st = buffer;
-	char *end = buffer;
 
+	char *string;
 	char *digits = "0123456789ABCDEF";
 
+	string = malloc(32 * sizeof(unsigned int));
+	if (!string)
+		exit(1);
+
+	char *st = string;
+	char *end = string;
+
 	do {
-		tmp_value = value;
+		*st = digits[value % 16];
 		value /= 16;
-		*st = digits[tmp_value % 16];
 		st++;
 	} while (value);
 
+	*st = '\0';
 	st--;
 
-	while (end < st)
+	while (*end)
 	{
-		tmp_char = *st;
-		*st = *end;
+		update_buff(st, buffer, 1);
 		st--;
-		*end = tmp_char;
 		end++;
 	}
+	free(string);
 }
