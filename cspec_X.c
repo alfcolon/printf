51ecda31abf67c950d0fa407ee7eb58c6a98dc4d
jmtes <juliennetesoro@gmail.com>
2019-03-19 20:36:31 +0000

    Seaprate declarations and code

--- cspec_X.c
+++ cspec_X.c
@@ -15,8 +15,9 @@ void cspec_X(void *val, char *buffer)
 {
 	unsigned int *valu = val;
 	unsigned int value = *valu;
-	char tmp_char;
 
+	char *st;
+	char *end;
 	char *string;
 	char *digits = "0123456789ABCDEF";
 
@@ -24,8 +25,8 @@ void cspec_X(void *val, char *buffer)
 	if (!string)
 		exit(1);
 
-	char *st = string;
-	char *end = string;
+	st = string;
+	end = string;
 
 	do {
 		*st = digits[value % 16];
