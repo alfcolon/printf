6ad41fc92d9914309c104d61bfcbd37112550394
jmtes <juliennetesoro@gmail.com>
2019-03-20 22:20:05 -0700

    Change function prototypes to take in param va_list

--- holberton.h
+++ holberton.h
@@ -7,8 +7,9 @@
 #include <string.h>
 /* constants */
 #include <unistd.h>
-
-#define BUFF_SIZE 1024
+#include <limits.h>
+#include <stddef.h>
+#include <stdlib.h>
 
 /**
  * struct mods - struct
@@ -19,24 +20,33 @@
 typedef	struct mods
 {
 	char	*spec;
-	int	(*f)(void *, char buff[BUFF_SIZE]);
+	int	(*f)(va_list, char *buff);
 }	t_array;
 
 int	_printf(const char *format, ...);
-int	print(char *format, va_list list, char buff[BUFF_SIZE]);
-int	(*mod(char cspec))(void *arg, char buff[BUFF_SIZE]);
+int	print(char *format, va_list list, char *buff);
+int	(*mod(char cspec))(va_list, char *buff);
 /* helper mod functions */
-int	cspec_b(void *arg, char buff[BUFF_SIZE]);
-int	cspec_c(void *arg, char buff[BUFF_SIZE]);
-int	cspec_d_i(void *arg, char buff[BUFF_SIZE]);
-int	cspec_s(void *arg, char buff[BUFF_SIZE]);
-int	cspec_r(void *arg, char buff[BUFF_SIZE]);
-int	cspec_R(void *arg, char buff[BUFF_SIZE]);
+int	cspec_b(va_list, char *buff);
+int	cspec_c(va_list, char *buff);
+int	cspec_d_i(va_list, char *buff);
+int	cspec_o(va_list, char *buff);
+int	cspec_p(va_list, char *buff);
+int	cspec_r(va_list, char *buff);
+int	cspec_R(va_list, char *buff);
+int	cspec_s(va_list, char *buff);
+int	cspec_S(va_list, char *buff);
+int	cspec_u(va_list, char *buff);
+int	cspec_x(va_list, char *buff);
+int	cspec_X(va_list, char *buff);
 /* general helper functions */
+int	_putchar(int c);
+void	_memset(char *s, char b, int n);
 char	*rot13(char *s);
 char	*rev_string(char *s);
 int	valid_cspec(char c);
 int	_strlen(char *s);
-void	update_buff(char *arg, char buff[BUFF_SIZE], int len);
+int     hex(char *arr, char c, int i);
+void	update_buff(char *arg, char *buff, int len);
 int	error(char *format, va_list copy);
 #endif
