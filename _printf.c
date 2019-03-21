3fe7969dfcd647b25e6088956c441faf0de2a6df
jmtes <juliennetesoro@gmail.com>
2019-03-20 22:14:00 -0700

    Copy from other branch

---
+++ _printf.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * _printf - driver function for printf remake
+ * @format: holds text and connversion specifiers
+ * @...: variadic arguments
+ *
+ * Description: checks for errror, prints buff, returns sum
+ * Return: sum of bytes written
+ */
+int     _printf(const char *format, ...)
+{
+	char    buff[1024];
+	int	printed = 0;
+	va_list list;
+
+	if (!format)
+		return (-1);
+	va_start(list, format);
+	/*set buff elements to 0*/
+	_memset((void *)buff, 0, 1024);
+	/* get # of printed characters from print function */
+	printed = print((char *)format, list, buff);
+	va_end(list);
+	return (printed);
+}
